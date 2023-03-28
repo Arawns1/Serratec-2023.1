85813ce3cf66eb65c4e9157293389ff1601c2aad
Gabriel Damico <gabrieldamico22@gmail.com>
2023-03-28 18:00:40 -0300

    Criação da nota fiscal

--- Banco de Dados/Trabalho Ecommerce/consultas com join.sql
+++ Banco de Dados/Trabalho Ecommerce/consultas com join.sql
@@ -1,7 +1,14 @@
-select * from pedido
+select * from pedido 
 select * from vendedor_produto
 
-select p.data_compra, u.nome as "cliente", p.id as "pedido id", pp.id_produto, pr.nome as "nome produto" , pr.valor_unitario,  pp.quantidade_produto from pedido_produto pp
+select p.data_compra, u.nome as "cliente", 
+	   p.id as "pedido id", 
+	   vp.id_vendedor as "vendedor",
+	   pp.id_produto, pr.nome as "nome produto",
+	   pr.valor_unitario as "preço unitario",  
+	   pp.quantidade_produto as "quantidade", 
+	   valor_unitario * quantidade_produto as "total"
+from pedido_produto pp
 
 join pedido p
 on pp.id_pedido = p.id 
