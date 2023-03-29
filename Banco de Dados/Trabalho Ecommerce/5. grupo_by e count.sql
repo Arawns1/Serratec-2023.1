aa04bb26edcc74c94ea70506f2bda3af2c75d987
Gabriel D <62900084+Arawns1@users.noreply.github.com>
2023-03-29 17:17:37 -0300

    Update 5. grupo_by e count.sql

--- Banco de Dados/Trabalho Ecommerce/5. grupo_by e count.sql
+++ Banco de Dados/Trabalho Ecommerce/5. grupo_by e count.sql
@@ -1,6 +1,11 @@
 -- Count() - conta a quantidade de pedidos
-select Count(id) from pedido
+select Count(id) as numero_pedidos from pedido;
+
+-- Adicionando um novo produto para aumentar o número de produtos por categoria
+insert into produto(nome, descricao, data_fabricacao, valor_unitario, id_categoria)
+values ("Celular","Hi-Tech", "12/09/2023", 100, 2);
 
 -- Group by () - Fala quantos produtos tem por categoria
-select Count(id_categoria) from produto p 
-group by p.id_categoria
+select c.nome, Count(id_categoria) as produtos_categoria from produto p 
+inner join categoria c on c.id = p.id_categoria 
+group by p.id_categoria;
