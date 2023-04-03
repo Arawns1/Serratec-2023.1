f42e9e17460e1269b1f3698b326460d54d66a066
Gabriel Damico <gabrieldamico22@gmail.com>
2023-04-03 17:54:46 -0300

    JAVA POO Organizado

---
+++ Java POO/Aula 1 e 2/src/Aula01/BalancoTrimestral.java
@@ -0,0 +1,15 @@
+package Aula01;
+
+public class BalancoTrimestral {
+
+	public static void main(String[] args) {
+		int gastosJaneiro = 15000;
+		int gastosFevereiro =23000;
+		int gastosMarco = 17000;
+		
+		int gastosTrimestre = gastosJaneiro+gastosFevereiro+gastosMarco;
+		
+		System.out.println("O total de gastos do primeiro trimestre foi de: R$" + gastosTrimestre);
+	}
+
+}
