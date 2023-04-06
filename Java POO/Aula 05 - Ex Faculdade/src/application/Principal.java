ecad35fef71aac3edf45e5ba20f5b134889d0f16
Gabriel Damico <gabrieldamico22@gmail.com>
2023-04-06 17:53:59 -0300

    RelatorioDeGastos como Static

--- Java POO/Aula 05 - Ex Faculdade/src/application/Principal.java
+++ Java POO/Aula 05 - Ex Faculdade/src/application/Principal.java
@@ -4,18 +4,17 @@ import entities.Empregado;
 import entities.Professor;
 import entities.RelatorioDeGastos;
 
-public class main {
-
+public class Principal {
+	
 	public static void main(String[] args) {
 		Empregado emp = new Empregado("Gabriel", 1000);
 		Professor prof = new Professor("José", 1000, 10);
+		Professor prof2 = new Professor("Pedro", 1200, 30);
 		
 		System.out.println(emp.getInfo());
 		System.out.println(prof.getInfo());
 		
-		RelatorioDeGastos RG = new RelatorioDeGastos();
-		RG.calculaGastos(emp, prof);
-
+		RelatorioDeGastos.calculaGastos(emp, prof, prof2);
 	}
 
 }
