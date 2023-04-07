b873580515f2e7c3200aadcfb112dbf972b631e9
Gabriel Damico <gabrieldamico22@gmail.com>
2023-04-06 21:50:49 -0300

    Voltei com o pedro

--- Java POO/Aula 05 - Ex Faculdade/src/application/Principal.java
+++ Java POO/Aula 05 - Ex Faculdade/src/application/Principal.java
@@ -9,7 +9,7 @@ public class Principal {
 	public static void main(String[] args) {
 		Empregado emp = new Empregado("Gabriel", 1000);
 		Professor prof = new Professor("José", 1000, 10);
-		
+		Professor prof2 = new Professor("Pedro", 1200, 30);
 		
 		System.out.println(emp.getInfo());
 		System.out.println(prof.getInfo());
