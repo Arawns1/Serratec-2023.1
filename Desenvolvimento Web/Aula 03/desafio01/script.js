2f650373ee802c155573a8efe73af724c7356edc
Gabriel Damico <gabrieldamico22@gmail.com>
2023-04-28 19:13:01 -0300

    com texto mudando

---
+++ Desenvolvimento Web/Aula 03/desafio01/script.js
@@ -0,0 +1,16 @@
+var textHeader = ["Futuro Desenvolvedor FullStack","Residente Serratec 2023.1","Cursando Ciência da Computação"]
+  
+var j = 0;
+  var counter = 0;
+  var i = setInterval(function(){
+  var textoh3 = document.getElementById("title-text");
+  textoh3.innerHTML = `<h3 id="title-text"> ${textHeader[j]}</h3>`;
+      counter++;
+      if(counter === 900) {
+          clearInterval(i);
+      }
+      j++;
+      if(j === 3){
+        j=0
+      }
+  }, 3000);
