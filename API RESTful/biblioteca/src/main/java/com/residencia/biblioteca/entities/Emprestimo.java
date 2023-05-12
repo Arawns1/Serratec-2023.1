ea6d6394043c1983066f49cacca6052b7ba6f821
Gabriel Damico <gabrieldamico22@gmail.com>
2023-05-11 22:12:32 -0300

    Correção de Imports

--- API RESTful/biblioteca/src/main/java/com/residencia/biblioteca/entities/Emprestimo.java
+++ API RESTful/biblioteca/src/main/java/com/residencia/biblioteca/entities/Emprestimo.java
@@ -2,7 +2,6 @@ package com.residencia.biblioteca.entities;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -12,7 +11,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
